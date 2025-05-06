b020e1e2211a5bac92af414b2bd6b07965766519
jhanviaggarwal <jhanviaggarwal1610@gmail.com>
2025-05-06 11:31:26 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>welcome to react app with implement ci/cd pipelines</h1>
+        <h2>with github action</h2>
+        <h3>by jhanvi aggarwal</h3>
+        </div>
+    </>
+  )
+}
+
+export default App
